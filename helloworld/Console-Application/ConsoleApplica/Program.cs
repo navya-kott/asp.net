2ea9df6dc8901dc7aad6f1706e0d7a73ba65f239
navya <navya.ak@kottsoftware.com>
2025-03-03 15:06:02 +0530

    csharp learning

---
+++ helloworld/Console-Application/ConsoleApplica/Program.cs
@@ -0,0 +1,18 @@
+﻿using Library;
+
+namespace ConsoleApplica
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+           int a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+           int b= Convert.ToInt32(Console.ReadLine());
+
+            Mathu.Addition(a, b);
+
+        }
+    }
+}
